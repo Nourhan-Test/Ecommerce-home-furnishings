19f102c4bb26d57e236ad170a4c42a9e2391f92d
Nourhan <nor272001@gmail.com>
2023-08-16 23:35:02 +0200

    finish home and shope page

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    screens:{
+        sm:'567px',
+        md:'768px',
+        lg:'992px',
+        xl:'1200px'
+
+    },
+    container:{
+       center:true,
+       padding:'1rem'
+
+    },
+    extend: {
+      fontFamily:{
+        poppins:"'Poppins',sans-serif",
+        roboto:"'Roboto', sans-serif"
+      },
+      colors:{
+        'primary':'#FD3D57'
+      }
+    },
+  },
+  variants:{
+extend:{
+  display:"group-hover",
+}
+  },
+  plugins: [],
+}
+
