3de88c56b8b63e0c0dcde5f49480c5adcb41790b
Nourhan <nor272001@gmail.com>
2023-08-17 18:28:02 +0200

    finish product detailes page

---
+++ main.js
@@ -0,0 +1,22 @@
+var MainImg=document.getElementById("MainImg");
+var smImg=document.getElementsByClassName("small-img");
+
+smImg[0].onclick=function(){
+    MainImg.src=smImg[0].src;
+}
+
+smImg[1].onclick=function(){
+    MainImg.src=smImg[1].src;
+}
+
+smImg[2].onclick=function(){
+    MainImg.src=smImg[2].src;
+}
+
+smImg[3].onclick=function(){
+    MainImg.src=smImg[3].src;
+}
+
+smImg[4].onclick=function(){
+    MainImg.src=smImg[4].src;
+}
